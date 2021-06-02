d0f69c6a126e703ab3aac89b7f9be0b20439e18b
daghanevzat <58523575+daghanevzat@users.noreply.github.com>
2021-06-02 16:52:33 +0300

    first commit

---
+++ Assets/DataFiles/Scripts/ChildBook.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ChildBook : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject cengizhanBookGo, daliBookGo, eserlerBookGo, haliBookGo, kitapBookGo, picassoBookGo, rodinBookGo, venedikBookGo, caterpillarBookGo, corduroyBookGo, mooseBookGo, numbersGo, colorsGo, animalsGo;
+
+    void Start()
+    {
+        VirtualButtonBehaviour[] vrb = GetComponents<VirtualButtonBehaviour> ();
+        for (int i = 0; i < vrb.Length; i++)
+        {
+            vrb[i].RegisterEventHandler(this);
+        }
+        cengizhanBookGo.SetActive(false);
+        daliBookGo.SetActive(false);
+        eserlerBookGo.SetActive(false);
+        haliBookGo.SetActive(false);
+        kitapBookGo.SetActive(false);
+        picassoBookGo.SetActive(false);
+        rodinBookGo.SetActive(false);
+        venedikBookGo.SetActive(false);
+        caterpillarBookGo.SetActive(false);
+        corduroyBookGo.SetActive(false);
+        mooseBookGo.SetActive(false);
+        numbersGo.SetActive(false);
+        colorsGo.SetActive(false);
+        animalsGo.SetActive(false);
+    }
+    
+    void Update()
+    {
+
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if (vb.VirtualButtonName == "CengizhanBookVB")
+        {
+            Debug.Log("Cengizhan Book Button pressed.");
+            cengizhanBookGo.SetActive(true);
+            daliBookGo.SetActive(false);
+            eserlerBookGo.SetActive(false);
+            haliBookGo.SetActive(false);
+            kitapBookGo.SetActive(false);
+            picassoBookGo.SetActive(false);
+            rodinBookGo.SetActive(false);
+            venedikBookGo.SetActive(false);
+            caterpillarBookGo.SetActive(false);
+            corduroyBookGo.SetActive(false);
+            mooseBookGo.SetActive(false);
+            numbersGo.SetActive(false);
+            colorsGo.SetActive(false);
+            animalsGo.SetActive(false);
+        }
+        else if (vb.VirtualButtonName == "DaliButtonVB")
+        {
+            Debug.Log("Dali Book Button pressed.");
+            cengizhanBookGo.SetActive(false);
+            daliBookGo.SetActive(true);
+            eserlerBookGo.SetActive(false);
+            haliBookGo.SetActive(false);
+            kitapBookGo.SetActive(false);
+            picassoBookGo.SetActive(false);
+            rodinBookGo.SetActive(false);
+            venedikBookGo.SetActive(false);
+            caterpillarBookGo.SetActive(false);
+            corduroyBookGo.SetActive(false);
+            mooseBookGo.SetActive(false);
+            numbersGo.SetActive(false);
+            colorsGo.SetActive(false);
+            animalsGo.SetActive(false);
+        }
+        else if (vb.VirtualButtonName == "EserlerButtonVB")
+        {
+            Debug.Log("Eserler Book Button pressed.");
+            cengizhanBookGo.SetActive(false);
+            daliBookGo.SetActive(false);
+            eserlerBookGo.SetActive(true);
+            haliBookGo.SetActive(false);
+            kitapBookGo.SetActive(false);
+            picassoBookGo.SetActive(false);
+            rodinBookGo.SetActive(false);
+            venedikBookGo.SetActive(false);
+            caterpillarBookGo.SetActive(false);
+            numbersGo.SetActive(false);
+            colorsGo.SetActive(false);
+            animalsGo.SetActive(false);
+        }
+        else if (vb.VirtualButtonName == "HaliButtonVB")
+        {
+            Debug.Log("Hali Book Button pressed.");
+            cengizhanBookGo.SetActive(false);
+            daliBookGo.SetActive(false);
+            eserlerBookGo.SetActive(false);
+            haliBookGo.SetActive(true);
+            kitapBookGo.SetActive(false);
+            picassoBookGo.SetActive(false);
+            rodinBookGo.SetActive(false);
+            venedikBookGo.SetActive(false);
+            caterpillarBookGo.SetActive(false);
+            corduroyBookGo.SetActive(false);
+            mooseBookGo.SetActive(false);
+            numbersGo.SetActive(false);
+            colorsGo.SetActive(false);
+            animalsGo.SetActive(false);
+        }
+        else if (vb.VirtualButtonName == "KitapButtonVB")
+        {
+            Debug.Log("Kitap Book Button pressed.");
+            cengizhanBookGo.SetActive(false);
+            daliBookGo.SetActive(false);
+            eserlerBookGo.SetActive(false);
+            haliBookGo.SetActive(false);
+            kitapBookGo.SetActive(true);
+            picassoBookGo.SetActive(false);
+            rodinBookGo.SetActive(false);
+            venedikBookGo.SetActive(false);
+            caterpillarBookGo.SetActive(false);
+            corduroyBookGo.SetActive(false);
+            mooseBookGo.SetActive(false);
+            numbersGo.SetActive(false);
+            colorsGo.SetActive(false);
+            animalsGo.SetActive(false);
+        }
+        else if (vb.VirtualButtonName == "PicassoButtonVB")
+        {
+            Debug.Log("Kitap Book Button pressed.");
+            cengizhanBookGo.SetActive(false);
+            daliBookGo.SetActive(false);
+            eserlerBookGo.SetActive(false);
+            haliBookGo.SetActive(false);
+            kitapBookGo.SetActive(false);
+            picassoBookGo.SetActive(true);
+            rodinBookGo.SetActive(false);
+            venedikBookGo.SetActive(false);
+            caterpillarBookGo.SetActive(false);
+            corduroyBookGo.SetActive(false);
+            mooseBookGo.SetActive(false);
+            numbersGo.SetActive(false);
+            colorsGo.SetActive(false);
+            animalsGo.SetActive(false);
+        }
+        else if (vb.VirtualButtonName == "RodinButtonVB")
+        {
+            Debug.Log("Rodin Book Button pressed.");
+            cengizhanBookGo.SetActive(false);
+            daliBookGo.SetActive(false);
+            eserlerBookGo.SetActive(false);
+            haliBookGo.SetActive(false);
+            kitapBookGo.SetActive(false);
+            picassoBookGo.SetActive(false);
+            rodinBookGo.SetActive(true);
+            venedikBookGo.SetActive(false);
+            caterpillarBookGo.SetActive(false);
+            corduroyBookGo.SetActive(false);
+            mooseBookGo.SetActive(false);
+            numbersGo.SetActive(false);
+            colorsGo.SetActive(false);
+            animalsGo.SetActive(false);
+        }
+        else if (vb.VirtualButtonName == "VenedikButtonVB")
+        {
+            Debug.Log("Venedik Book Button pressed.");
+            cengizhanBookGo.SetActive(false);
+            daliBookGo.SetActive(false);
+            eserlerBookGo.SetActive(false);
+            haliBookGo.SetActive(false);
+            kitapBookGo.SetActive(false);
+            picassoBookGo.SetActive(false);
+            rodinBookGo.SetActive(false);
+            venedikBookGo.SetActive(true);
+            caterpillarBookGo.SetActive(false);
+            corduroyBookGo.SetActive(false);
+            mooseBookGo.SetActive(false);
+            numbersGo.SetActive(false);
+            colorsGo.SetActive(false);
+            animalsGo.SetActive(false);
+        }
+        else if (vb.VirtualButtonName == "CaterpillarButtonVB")
+        {
+            Debug.Log("Caterpillar Book Button pressed.");
+            cengizhanBookGo.SetActive(false);
+            daliBookGo.SetActive(false);
+            eserlerBookGo.SetActive(false);
+            haliBookGo.SetActive(false);
+            kitapBookGo.SetActive(false);
+            picassoBookGo.SetActive(false);
+            rodinBookGo.SetActive(false);
+            venedikBookGo.SetActive(false);
+            caterpillarBookGo.SetActive(true);
+            corduroyBookGo.SetActive(false);
+            mooseBookGo.SetActive(false);
+            numbersGo.SetActive(false);
+            colorsGo.SetActive(false);
+            animalsGo.SetActive(false);
+        }
+        else if (vb.VirtualButtonName == "CorduroyButtonVB")
+        {
+            Debug.Log("Corduroy Book Button pressed.");
+            cengizhanBookGo.SetActive(false);
+            daliBookGo.SetActive(false);
+            eserlerBookGo.SetActive(false);
+            haliBookGo.SetActive(false);
+            kitapBookGo.SetActive(false);
+            picassoBookGo.SetActive(false);
+            rodinBookGo.SetActive(false);
+            venedikBookGo.SetActive(false);
+            caterpillarBookGo.SetActive(false);
+            corduroyBookGo.SetActive(true);
+            mooseBookGo.SetActive(false);
+            numbersGo.SetActive(false);
+            colorsGo.SetActive(false);
+            animalsGo.SetActive(false);
+        }
+        else if (vb.VirtualButtonName == "MooseMuffinButtonVB")
+        {
+            Debug.Log("Moose Muffin Book Button pressed.");
+            cengizhanBookGo.SetActive(false);
+            daliBookGo.SetActive(false);
+            eserlerBookGo.SetActive(false);
+            haliBookGo.SetActive(false);
+            kitapBookGo.SetActive(false);
+            picassoBookGo.SetActive(false);
+            rodinBookGo.SetActive(false);
+            venedikBookGo.SetActive(false);
+            caterpillarBookGo.SetActive(false);
+            corduroyBookGo.SetActive(false);
+            mooseBookGo.SetActive(true);
+            numbersGo.SetActive(false);
+            colorsGo.SetActive(false);
+            animalsGo.SetActive(false);
+        }
+        else
+        {
+            throw new UnityException(vb.VirtualButtonName + "Virtual Button not Supported");
+        }
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Virtual Button released.");
+    }
+
+}
---
+++ Assets/DataFiles/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,212 @@
+﻿/*==============================================================================
+Copyright (c) 2019 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// A custom handler that implements the ITrackableEventHandler interface.
+///
+/// Changes made to this file could be overwritten when upgrading the Vuforia version.
+/// When implementing custom event handler behavior, consider inheriting from this class instead.
+/// </summary>
+public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    #region PROTECTED_MEMBER_VARIABLES
+    public GameObject cengizhanBookGo, daliBookGo, eserlerBookGo, haliBookGo, kitapBookGo, picassoBookGo, rodinBookGo, venedikBookGo, caterpillarBookGo, corduroyBookGo, mooseBookGo, numbersGo, colorsGo, animalsGo;
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+    #endregion // PROTECTED_MEMBER_VARIABLES
+
+    #region UNITY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        cengizhanBookGo.SetActive(false);
+        daliBookGo.SetActive(false);
+        eserlerBookGo.SetActive(false);
+        haliBookGo.SetActive(false);
+        kitapBookGo.SetActive(false);
+        picassoBookGo.SetActive(false);
+        rodinBookGo.SetActive(false);
+        venedikBookGo.SetActive(false);
+        caterpillarBookGo.SetActive(false);
+        corduroyBookGo.SetActive(false);
+        mooseBookGo.SetActive(false);
+        numbersGo.SetActive(false);
+        colorsGo.SetActive(false);
+        animalsGo.SetActive(false);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName +
+                  " " + mTrackableBehaviour.CurrentStatus +
+                  " -- " + mTrackableBehaviour.CurrentStatusInfo);
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            if (mTrackableBehaviour.TrackableName =="Numbers")
+            {
+                numbersGo.SetActive(true);
+                colorsGo.SetActive(false);
+                animalsGo.SetActive(false);
+                cengizhanBookGo.SetActive(false);
+                daliBookGo.SetActive(false);
+                eserlerBookGo.SetActive(false);
+                haliBookGo.SetActive(false);
+                kitapBookGo.SetActive(false);
+                picassoBookGo.SetActive(false);
+                rodinBookGo.SetActive(false);
+                venedikBookGo.SetActive(false);
+                caterpillarBookGo.SetActive(false);
+                corduroyBookGo.SetActive(false);
+                mooseBookGo.SetActive(false);
+            }
+            else if (mTrackableBehaviour.TrackableName == "Color")
+            {
+                numbersGo.SetActive(false);
+                colorsGo.SetActive(true);
+                animalsGo.SetActive(false);
+                cengizhanBookGo.SetActive(false);
+                daliBookGo.SetActive(false);
+                eserlerBookGo.SetActive(false);
+                haliBookGo.SetActive(false);
+                kitapBookGo.SetActive(false);
+                picassoBookGo.SetActive(false);
+                rodinBookGo.SetActive(false);
+                venedikBookGo.SetActive(false);
+                caterpillarBookGo.SetActive(false);
+                corduroyBookGo.SetActive(false);
+                mooseBookGo.SetActive(false);
+            }
+            else if (mTrackableBehaviour.TrackableName == "Animals")
+            {
+                numbersGo.SetActive(false);
+                colorsGo.SetActive(false);
+                animalsGo.SetActive(true);
+                cengizhanBookGo.SetActive(false);
+                daliBookGo.SetActive(false);
+                eserlerBookGo.SetActive(false);
+                haliBookGo.SetActive(false);
+                kitapBookGo.SetActive(false);
+                picassoBookGo.SetActive(false);
+                rodinBookGo.SetActive(false);
+                venedikBookGo.SetActive(false);
+                caterpillarBookGo.SetActive(false);
+                corduroyBookGo.SetActive(false);
+                mooseBookGo.SetActive(false);
+            }
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            cengizhanBookGo.SetActive(false);
+            daliBookGo.SetActive(false);
+            eserlerBookGo.SetActive(false);
+            haliBookGo.SetActive(false);
+            kitapBookGo.SetActive(false);
+            picassoBookGo.SetActive(false);
+            rodinBookGo.SetActive(false);
+            venedikBookGo.SetActive(false);
+            caterpillarBookGo.SetActive(false);
+            corduroyBookGo.SetActive(false);
+            mooseBookGo.SetActive(false);
+            numbersGo.SetActive(false);
+            colorsGo.SetActive(false);
+            animalsGo.SetActive(false);
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PROTECTED_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        if (mTrackableBehaviour)
+        {
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            // Enable rendering:
+            foreach (var component in rendererComponents)
+                component.enabled = true;
+
+            // Enable colliders:
+            foreach (var component in colliderComponents)
+                component.enabled = true;
+
+            // Enable canvas':
+            foreach (var component in canvasComponents)
+                component.enabled = true;
+        }
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        if (mTrackableBehaviour)
+        {
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            // Disable rendering:
+            foreach (var component in rendererComponents)
+                component.enabled = false;
+
+            // Disable colliders:
+            foreach (var component in colliderComponents)
+                component.enabled = false;
+
+            // Disable canvas':
+            foreach (var component in canvasComponents)
+                component.enabled = false;
+        }
+    }
+
+    #endregion // PROTECTED_METHODS
+}
